8e4bbf7525155be78fb1816e9e338dbadd391c4f
amit <ashethe@deqode.com>
2021-09-28 11:41:21 +0530

    intial commit

---
+++ Day1.2.rb
@@ -0,0 +1,6 @@
+arr = [];
+if arr.length > 0;
+puts "1"
+else
+    puts "0"
+end
---
+++ Day1.4.rb
@@ -0,0 +1,31 @@
+# class Abcd
+
+# def doublearray
+#     a1 = ["1","2"];
+#     a1 = a1 + a1;
+#     a1.each do |i|
+#         puts i
+#     end
+    
+# end
+# Abcd.doublearray()
+# end
+# class Amit
+#     def doublearray1
+#         a1 = ["1","2"]
+#         a1 = a1 + a1;
+#         a1.each do |i|
+#             puts i
+#         end
+#     end
+#     Amit.doublearray1
+# end
+def say
+    a1 = ["1","2"]
+    a1 = a1 + a1;
+    a1.each do |i|
+        puts i
+  end
+  end
+  
+  say
